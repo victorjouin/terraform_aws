4a7b217ca783f14ddcfc7b572d8d8a9e30b0dfce
victor jouin <77397820+victorjouin@users.noreply.github.com>
2024-07-27 11:42:37 +0200

    gogo

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,7 +3,7 @@ provider "aws" {
 }
 
 resource "aws_instance" "example" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-05fa00d4c63e32376"
   instance_type = "t2.micro"
 
   tags = {
